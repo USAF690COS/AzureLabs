13c917c17cedec4bd7546c131ca4c058e36e06e7
Kevin Dillon <kevin.dillon@microsoft.com>
2020-04-13 12:25:37 -1000

    Linked Template Deoployment

--- ArmTemplates/Deploy Lab/labDeploy.ps1
+++ ArmTemplates/Deploy Lab/labDeploy.ps1
@@ -1,4 +1,4 @@
 $templateParamterFile = "C:\git\AzureLabs\ArmTemplates\Deploy Lab\azuredeploy.parameters.json"
 $templateFile = "C:\git\AzureLabs\ArmTemplates\Deploy Lab\azuredeploy.json"
 
-New-AzDeployment -name 'DeployLab' -Location 'westus' -TemplateParameterFile $templateParamterFile -TemplateFile $templateFile
+New-AzSubscriptionDeployment -Location 'westus' -name 'TestLab2' -TemplateParameterFile $templateParamterFile -TemplateFile $templateFile
