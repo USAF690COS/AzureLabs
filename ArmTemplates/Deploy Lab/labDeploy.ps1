33b1d5c0f32c10ef5c3bef952eee6349691e338d
Kevin Dillon <kevin.dillon@microsoft.com>
2020-04-14 17:05:14 -1000

    Updated ipconfig recourceid ref

--- ArmTemplates/Deploy Lab/labDeploy.ps1
+++ ArmTemplates/Deploy Lab/labDeploy.ps1
@@ -1,4 +1,4 @@
 $templateParamterFile = "C:\git\AzureLabs\ArmTemplates\Deploy Lab\azuredeploy.parameters.json"
 $templateFile = "C:\git\AzureLabs\ArmTemplates\Deploy Lab\azuredeploy.json"
 
-New-AzSubscriptionDeployment -Location 'westus' -name 'TestWestUS' -TemplateParameterFile $templateParamterFile -TemplateFile $templateFile
+New-AzSubscriptionDeployment -Location 'westus' -name 'TestWestUS2' -TemplateParameterFile $templateParamterFile -TemplateFile $templateFile
