bcfbfb91592e292e6a900b6c79f092911a5ca822
Kevin Dillon <kevin.dillon@microsoft.com>
2020-04-16 13:32:10 -1000

    Updated snapshot refs to copied snaps in westus region for testing

--- ArmTemplates/Deploy Lab/labDeploy.ps1
+++ ArmTemplates/Deploy Lab/labDeploy.ps1
@@ -1,4 +1,5 @@
 $templateParamterUri = "https://raw.githubusercontent.com/USAF690COS/AzureLabs/master/ArmTemplates/Deploy%20Lab/azuredeploy.parameters.json"
+$templateParamterFile = "C:\git\AzureLabs\ArmTemplates\Deploy Lab\azuredeploy.parameters.json"
 $templateUri = "https://raw.githubusercontent.com/USAF690COS/AzureLabs/master/ArmTemplates/Deploy%20Lab/azuredeploy.json"
 
-New-AzSubscriptionDeployment -Location 'westus2' -name 'LabTestWestUS2' -TemplateParameterUri $templateParamterUri -TemplateUri $templateUri
+New-AzSubscriptionDeployment -Location 'westus2' -name 'LabTestWestUS2' -TemplateUri $templateUri -TemplateParameterFile $templateParamterFile
