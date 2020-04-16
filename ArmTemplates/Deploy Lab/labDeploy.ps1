62cd5d060e4d062cfe2f8c846c585fe3693ea8ee
Kevin Dillon <kevin.dillon@microsoft.com>
2020-04-16 12:22:13 -1000

    Updated snapshot reference for DC1 & 2

--- ArmTemplates/Deploy Lab/labDeploy.ps1
+++ ArmTemplates/Deploy Lab/labDeploy.ps1
@@ -1,4 +1,4 @@
-$templateParamterFile = "C:\git\AzureLabs\ArmTemplates\Deploy Lab\azuredeploy.parameters.json"
-$templateFile = "C:\git\AzureLabs\ArmTemplates\Deploy Lab\azuredeploy.json"
+$templateParamterUri = "https://raw.githubusercontent.com/USAF690COS/AzureLabs/master/ArmTemplates/Deploy%20Lab/azuredeploy.parameters.json"
+$templateUri = "https://raw.githubusercontent.com/USAF690COS/AzureLabs/master/ArmTemplates/Deploy%20Lab/azuredeploy.json"
 
-New-AzSubscriptionDeployment -Location 'westus' -name 'LabTestWestUS' -TemplateParameterFile $templateParamterFile -TemplateFile $templateFile
+New-AzSubscriptionDeployment -Location 'westus' -name 'LabTestWestUS' -TemplateParameterUri $templateParamterUri -TemplateUri $templateUri
