243c94eed83d4d2cfa0b2882f0b83001e2aebedc
Kevin Dillon <kevin.dillon@microsoft.com>
2020-04-20 11:06:08 -1000

    Updated Backup/snapshot scripts

--- PowerShell/BackupVMs.ps1
+++ PowerShell/BackupVMs.ps1
@@ -1,53 +1,45 @@
-# Creates snapshot of each VM, then copies each snapshot to destination storage account container
-# Need to update storageaccount key to use keyvault
-# need to modify loops - create all snapshots first (very fast), then loop through regions
-# to copy all snapshots to storage account in that region
-# - also fix storage context - needs to stay unique between storage accounts or copy fails
+# Creates snapshot of each VM, then copies each snapshot to destination storage account containers
 
 $subscription = Get-AzSubscription
 $SubscriptionId = $subscription.Id
 Select-AzSubscription -SubscriptionId $SubscriptionId
 
 $regions = "westus", "westus2"
-$rgNamePrefix = "vmImages-"
+# $rgNamePrefix = "vmImages-"
 $vms = "DC1", "DC2"
 $storageAccountPrefix = "vmimagevhds"
 $sourceResourceGroupName = "Lab-Kevin-DCPromo"
 $location = "westus"
+$destinationContext = @()
+$snapshotList = @()
+$sas = @()
 
+# Create snapshot of all VMs
 ForEach ($vmName in $vms) {
-    #create snapshot of each VM
     $timeStamp = Get-Date -Format "yyyyMMddHHmm"
     $snapshotName = $vmName + $timeStamp
-    $snapshotName
+    $snapshotList += $snapshotName
 
+    #Create Snapshot of VM and get SAS access token
     $vm = get-azvm -ResourceGroupName $sourceResourceGroupName -Name $vmName
-
     $snapshot =  New-AzSnapshotConfig -SourceUri $vm.StorageProfile.OsDisk.ManagedDisk.Id -Location $location -CreateOption copy
-
     New-AzSnapshot -Snapshot $snapshot -SnapshotName $snapshotName -ResourceGroupName $sourceResourceGroupName 
-
-    #Copy new snapshot to VMImages storage account in each region
-    ForEach ($region in $regions) {   
-        $targetResourceGroup = $rgNamePrefix + $region
-        $targetStorageAccountName = $storageAccountPrefix + $region
-        $targetStorageContainerName = "vmimages"
-        $sasExpiryDuration = "3600"
-        $destinationVHDFileName = "$snapshotName.vhd"
-
-        #**Need to change to keyvault
-        $storageAccountKey = Read-Host -Prompt "$targetStorageAccountName key:" 
-
-        #Generate the SAS for the snapshot 
-        $sas = Grant-AzSnapshotAccess -ResourceGroupName $sourceResourceGroupName -SnapshotName $snapshotName -DurationInSecond $sasExpiryDuration -Access Read
-
-        #Create the context for the storage account which will be used to copy snapshot to the storage account 
-        $destinationContext = New-AzStorageContext -StorageAccountName $targetStorageAccountName -StorageAccountKey $storageAccountKey
-
-        #Copy the snapshot to the storage account 
-        Start-AzStorageBlobCopy -AbsoluteUri $sas.AccessSAS -DestContainer $targetStorageContainerName -DestContext $destinationContext -DestBlob $destinationVHDFileName          
-    }
+    $sas += Grant-AzSnapshotAccess -ResourceGroupName $sourceResourceGroupName -SnapshotName $snapshotName -DurationInSecond $sasExpiryDuration -Access Read
 }
 
+# Copy new snapshot to VMImages storage account in each region
+For ($counter=0 ; $counter -lt $regions.Length; $counter++) { 
+    $targetStorageAccountName = $storageAccountPrefix + $regions[$counter]
+    $targetStorageContainerName = "vmimages"
+    $sasExpiryDuration = "3600"
+    $keyName = "snapStorageKey-" + $regions[$counter]    
+    $storageAccountKey = (Get-AzKeyVaultSecret -vaultName "USAF-690COS-LabKeys" -name $keyName).SecretValueText
+    $destinationContext += New-AzStorageContext -StorageAccountName $targetStorageAccountName -StorageAccountKey $storageAccountKey
 
-
+    For ($vmCount = 0; $vmCount -lt $snapshotList.Length; $vmCount++) {
+        #Copy the snapshot to the storage account 
+        $vmSnapName = $snapshotList[$vmCount]
+        $destinationVHDFileName = "$vmSnapName.vhd"
+        Start-AzStorageBlobCopy -AbsoluteUri $sas[$vmCount].AccessSAS -DestContainer $targetStorageContainerName -DestContext $destinationContext[$counter] -DestBlob $destinationVHDFileName          
+    }
+}
