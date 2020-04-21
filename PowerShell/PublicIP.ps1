ab9b6eecd67a8aa3cd8cc2e090ec452fc28e5e5b
Kevin Dillon <kevin.dillon@microsoft.com>
2020-04-20 15:42:35 -1000

    Updated ARM Template outputs and deployment script to output mstsc connections for each VM

--- PowerShell/PublicIP.ps1
+++ PowerShell/PublicIP.ps1
@@ -1,9 +1,3 @@
-$templateParamterUri = "https://raw.githubusercontent.com/USAF690COS/AzureLabs/master/ArmTemplates/Deploy%20Lab/azuredeploy.parameters.json"
-$templateParamterFile = "C:\git\AzureLabs\ArmTemplates\Deploy Lab\azuredeploy.parameters.json"
-$templateUri = "https://raw.githubusercontent.com/USAF690COS/AzureLabs/master/ArmTemplates/Deploy%20Lab/azuredeploy.json"
-
-$outputs = New-AzSubscriptionDeployment -Location 'westus' -name 'LabDeployWestUS' -TemplateUri $templateUri -TemplateParameterFile $templateParamterFile
-
 $resourceGroupName = $outputs.Outputs.resourceGroupName.Value
 $resourceGroupName
 
