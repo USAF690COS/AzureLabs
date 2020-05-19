451ad4ffecedd22a87b1d0e50eaeb27070da6736
Kevin Dillon <kevin.dillon@microsoft.com>
2020-05-18 15:13:26 -1000

    Depricated labDeploy ps script

--- ArmTemplates/Deploy Lab/labDeploy.ps1
+++ ArmTemplates/Deploy Lab/labDeploy.ps1
@@ -1,3 +1,8 @@
+# PLEASE USE THE AZURE AUTOMATION RUNBOOK - LabAutomation/DeployLab - TO CREATE SINGLE LAB INSTANCES, OR
+# USE THE PowerShell/Deployment Scripts/BatchDeployLab.ps1 SCRIPT TO CREATE SINGLE OR MULTIPLE LAB INSTANCES
+# THIS SCRIPT WILL BE REMOVED AT SOME POINT IN THE NEAR FUTURE
+
+<#
 $templateParamterFile = "C:\git\AzureLabs\ArmTemplates\Deploy Lab\azuredeploy.parameters.json"
 $templateUri = "https://raw.githubusercontent.com/USAF690COS/AzureLabs/master/ArmTemplates/Deploy%20Lab/azuredeploy.json"
 
@@ -16,3 +21,5 @@ For ($outputCount=0; $outputCount -lt $ipConfigValues.Count; $outputCount++) {
     $RDPConnection = $VMName + " - " + $PublicIP + ':' + $PublicPort
     $RDPConnection
 }
+
+#>
