201dcd2bed61b9993ad74ffe649511e349c1b365
Jon Castillo <jocasti@microsoft.com>
2020-06-17 16:39:13 -1000

    Updated net use command

--- Testing/CreateScheduledTask.ps1
+++ Testing/CreateScheduledTask.ps1
@@ -1,5 +1,5 @@
 #Define variables for Scheduled task
-$staction = New-ScheduledTaskAction -Execute 'Net' -Argument 'use S: \\st690coslab001.file.core.windows.net\scripts /u:st690coslab001 gBs1e1P7z7F7B8Ei/gFmbGA37nadjwsoRbZVLV95kFw825J6RRxmAEJlY+iqvsdstawQWahgw++nQ1WyilRmtg=='
+$staction = New-ScheduledTaskAction -Execute 'Net' -Argument 'use S: \\st690coslab001.file.core.windows.net\scripts /u:st690coslab001 gBs1e1P7z7F7B8Ei/gFmbGA37nadjwsoRbZVLV95kFw825J6RRxmAEJlY+iqvsdstawQWahgw++nQ1WyilRmtg== /persistent:yes'
 $stprin = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators"
 $sttrigger = New-ScheduledTaskTrigger -AtLogOn
 
