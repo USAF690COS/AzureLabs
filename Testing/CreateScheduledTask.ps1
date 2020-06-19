834d3bc7d523fea2b04f0aff3ec382a3a6df77f3
Jon Castillo <jocasti@microsoft.com>
2020-06-18 15:37:30 -1000

    Updated CreateScheduledTask script to copy files to local folder C:\scripts

--- Testing/CreateScheduledTask.ps1
+++ Testing/CreateScheduledTask.ps1
@@ -1,7 +1,15 @@
 #Define variables for Scheduled task
-$staction = New-ScheduledTaskAction -Execute 'Net' -Argument 'use S: \\st690coslab001.file.core.windows.net\scripts /u:st690coslab001 gBs1e1P7z7F7B8Ei/gFmbGA37nadjwsoRbZVLV95kFw825J6RRxmAEJlY+iqvsdstawQWahgw++nQ1WyilRmtg== /persistent:yes'
+
+#Array of actions for scheduled task
+$stactions = (New-ScheduledTaskAction -Execute 'Net' -Argument 'use S: \\st690coslab001.file.core.windows.net\scripts /u:st690coslab001 gBs1e1P7z7F7B8Ei/gFmbGA37nadjwsoRbZVLV95kFw825J6RRxmAEJlY+iqvsdstawQWahgw++nQ1WyilRmtg== /persistent:yes'),
+             (New-ScheduledTaskAction -Execute 'xcopy' -Argument 'S: C:\scripts\ /s /e /y')
+
+#Specify credentials for running the task
 $stprin = New-ScheduledTaskPrincipal -GroupId "BUILTIN\Administrators"
 $sttrigger = New-ScheduledTaskTrigger -AtLogOn
 
-#Create new scheduled task to map network share
-Register-ScheduledTask -Action $staction -Trigger $sttrigger -Principal $stprin -TaskName 'Map Scripts Share' -Force
+#Create a new task with these parameters
+$task = New-ScheduledTask -Action $stactions -Principal $stprin -Trigger $sttrigger
+
+#Register task with task scheduler service
+Register-ScheduledTask 'Stage Scripts' -InputObject $task -Force
